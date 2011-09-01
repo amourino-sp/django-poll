9049c2852d075079d7b7494f88bfa9ee606906d9
Pablo Monfort <pmonfort@security-pentest.com>
2011-09-01 19:46:34 -0300

    Add question detail class

--- project/poll/urls.py
+++ project/poll/urls.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-from .views import question_detail, question_list, question_add
-
 from django.conf.urls.defaults import patterns, url
+from .views import QuestionDetailView, question_detail, question_list, question_add
 
 urlpatterns = patterns('',
     url(r'^$', question_list, name='question_list'),
     url(r'^add$', question_add, name='question_add'),
-    url(r'^(?P<slug>[\w-]+)$', question_detail, name='question_detail'),
+    url(r'^(?P<slug>[\w-]+)$', QuestionDetailView.as_view(),
+        name='question_detail'),
+    # url(r'^(?P<slug>[\w-]+)$', question_detail, name='question_detail'),
 )
--- project/poll/views.py
+++ project/poll/views.py
@@ -1,17 +1,34 @@
-# -*- coding: utf-8 -*-
 from .models import Question, Option
-from .forms import QuestionForm
-
-from photologue.models import Photo
-
-from django.conf import settings
 from django.http import Http404
-from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+from poll.forms import QuestionForm
+from photologue.models import Photo
 from django.template.defaultfilters import slugify
+from django.views.generic import DetailView
+
+class QuestionDetailView(DetailView):
+    context_object_name = "question"
+    model = Question
+    template_name = "question_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+
+        context[self.context_object_name] = Question.objects.get(
+                                                slug=self.kwargs['slug'],
+                                            )
+        return context
 
 
 def question_detail(request, slug):
+    """
+    >>> a = ['larry', 'curly', 'moe']
+    >>> question_detail(a, 0)
+    'larry'
+    >>> question_detail(a, 1)
+    'curly'
+    """
     if request.method == 'POST':
         post = request.POST.copy()
         id = post.get("option_id", 0)
@@ -26,17 +43,21 @@ def question_detail(request, slug):
 
         return render_to_response(
             'question_detail.html',
-            {'question': question},
-            context_instance=RequestContext(request))
+            {'question':question, },
+            context_instance=RequestContext(request),
+        )
 
 
 def question_list(request):
     questions = Question.objects.all()
-    return render_to_response('question_list.html',
-                              {'questions': questions})
+    return render_to_response(
+                'question_list.html',
+                {'questions': questions, },
+    )
 
 
 def question_add(request):
+
     form = QuestionForm()
 
     if request.method == 'POST':
@@ -44,7 +65,8 @@ def question_add(request):
         form = QuestionForm(request.POST, request.FILES)
         photo = Photo(image=handle_uploaded_file(file_photo),
                       title=file_photo.name,
-                      title_slug=slugify(file_photo.name),)
+                      title_slug=slugify(file_photo.name),
+                )
         photo.save()
         form.photo = photo
         if form.is_valid():
@@ -54,12 +76,14 @@ def question_add(request):
 
     return render_to_response(
                 'question_add.html',
-                {'form': form},
-                context_instance=RequestContext(request))
+                {'form': form, },
+                context_instance=RequestContext(request),
+    )
 
 
 def handle_uploaded_file(f):
-    path = ''.join((settings.STATIC_ROOT, f.name[f.name.rfind('/') + 1:]))
+    path = ''.join(('/home/pablo/proyectos/clase/Encuesta/static/',
+           f.name[f.name.rfind('/') + 1:]))
     destination = open(path, 'wb+')
     for chunk in f.chunks():
         destination.write(chunk)
