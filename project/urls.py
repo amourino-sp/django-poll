c2c6faf4696dd89326290a8c60e5b706b3b1fe28
Alvaro Mouriño <alvaro@mourino.net>
2011-09-01 19:53:15 -0300

    Added settings import.

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
